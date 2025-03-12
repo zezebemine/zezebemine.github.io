5649d39532769702ea1e0a8c897c43efd7b40d18
zezebemine <jiaze.24@intl.zju.edu.cn>
2025-03-12 13:32:00 +0800

    Add files via upload

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,6 +1,10 @@
+# Input your weight in kg
 weight=float(input("enter your weight in kg:"))
-height=float(input("enter your height in m:67"))
+# Input your height in m
+height=float(input("enter your height in m:"))
+# Calculate the BMI value by the formula
 BMI=weight/height**2
+# Compare your BMI value with two critical values to judeg whether your BMI is normal or not.
 if BMI>30:
     print("obese")
 elif BMI<18.5:
